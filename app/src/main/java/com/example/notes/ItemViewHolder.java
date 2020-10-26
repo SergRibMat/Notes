efa70045de5b988029d0a1eef4b51443c1298321
sergio <serg.ribmat@gmail.com>
2020-10-26 10:44:52 +0100

    Delete unsused classes

--- app/src/main/java/com/example/notes/ItemViewHolder.java
+++
@@ -1,8 +0,0 @@
-package com.example.notes;
-
-public class ItemViewHolder {
-
-    private String name;
-
-
-}
--- app/src/main/java/com/example/notes/SearchViewListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.notes;
-
-
-
-public class SearchViewListener {
-
-
-}
