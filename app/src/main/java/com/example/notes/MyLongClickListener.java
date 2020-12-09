1e09697eb3b8a3c798bff0fb9e0762ca93388643
sergio <serg.ribmat@gmail.com>
2020-12-09 18:25:59 +0100

    remove unused code

--- app/src/main/java/com/example/notes/MyLongClickListener.java
+++ app/src/main/java/com/example/notes/MyLongClickListener.java
@@ -23,52 +23,6 @@ public class MyLongClickListener implements View.OnCreateContextMenuListener {
         this.myFilesHandler = myFilesHandler;
     }
 
-
-    public boolean onLongClick(View v) {
-        final Context context =v.getContext();
-        //para quitar txt tambien borrar el if
-        final int itemPosition = myRecycleView.getChildLayoutPosition(v);
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(MainActContext.getContext());
-        builder.setCancelable(true);
-        builder.setTitle(context.getString(R.string.deleteelement));
-        builder.setMessage(context.getString(R.string.deletequestion) + list.getItemAtPosition(itemPosition).toString());
-        builder.setNegativeButton(context.getString(R.string.cancel), new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                Toast.makeText(MainActContext.getContext(), context.getString(R.string.canceldelete), Toast.LENGTH_SHORT).show();
-                dialogInterface.cancel();
-            }
-        });
-
-        builder.setPositiveButton(context.getString(R.string.accept), new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                Section section = list.getItemAtPosition(itemPosition);
-                String name;
-                if(section instanceof Folder){
-                    name = section.getName();
-                }else{
-                    name = ((Note) section).getNameWithExtension();
-                }
-                adapterDataUpdater.removeSingleItem(itemPosition);
-                myFilesHandler.deleteFileOrFolder(name);
-            }
-        });
-        builder.show();
-        return true;//el return true es para indicar que no haga nada mas tras acabar esto. Si pone false, perfectamente
-        //lanza un onclickevent despues del longonclick
-    }
-
-    private void remane(){
-
-    }
-
-    private void delete(){
-
-    }
-
-
     @Override
     public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
         final int itemPosition = myRecycleView.getChildLayoutPosition(view);
