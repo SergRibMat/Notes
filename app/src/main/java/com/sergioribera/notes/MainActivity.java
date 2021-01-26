adbb8e5a1c9ad9d57ee142159a7c75c7bb6e83c4
sERGIO <serg.ribmat@gmail.com>
2021-01-26 18:00:59 +0100

    update version code

--- app/src/main/java/com/sergioribera/notes/MainActivity.java
+++ app/src/main/java/com/sergioribera/notes/MainActivity.java
@@ -91,8 +91,6 @@ public class MainActivity extends AppCompatActivity {
                     public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
 
                     }
-
-
                 }).check();
     }
 
